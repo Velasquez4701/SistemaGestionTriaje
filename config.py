42a0ec2d30039a22e552aae64cb84352295aa54c
SeN1313 <sebasnclass13@gmail.com>
2025-12-09 23:35:58 -0500

    Update encabezados de tabla
    Añadido DNI y modificado el mensaje de despedida

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ CARPETA_BASE = os.path.dirname(os.path.abspath(__file__))
 ARCHIVO_DB = os.path.join(CARPETA_BASE, "datos.json")
 
 ENCABEZADOS_TABLA = [
-    "Nombre", "Edad", "Sexo", "Peso (Kg)", "Talla (cm)", "IMC", "Clasificacion", "Presion", "Saturacion", "Atencion"
+    "DNI", "Nombre", "Edad", "Sexo", "Peso (Kg)", "Talla (cm)", "IMC", "Clasificacion", "Presion", "Saturacion", "Atencion"
 ]
 
 MSG_BIENVENIDA = "🏥 SISTEMA DE GESTION DE TRIAJE  - CLINICA SANTA MARIA"
-MSG_DESPEDIDA = "👋 ¡Gracias por utilizar el sistema! Datos guardados correctamente."
+MSG_DESPEDIDA = "✅ Datos guardados correctamente, ¡Gracias por utilizar el sistema! "
